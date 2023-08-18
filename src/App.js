c5314eb9798a5355c05fad42c3d4e5878d2d377f
Jakkaphat07 <110073963+Jakkaphat07@users.noreply.github.com>
2023-08-18 14:30:20 +0700

    calculator
    calculator

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {Header,Content,Footer} from './func-components'
 import Calendar from './class-components'
 import Banner from './banner'
+import {Calculator2} from './calculator'
 function App() {
     return (
         <>
@@ -9,6 +10,7 @@ function App() {
         <p><center><Calendar/></center></p>
         <Content/>
         <Banner/>
+        <Calculator2/> 
         <Footer/>
         </> 
        
