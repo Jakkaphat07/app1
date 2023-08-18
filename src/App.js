2202d4e60a231b10e7804761139aebd86c2de566
Jakkaphat07 <110073963+Jakkaphat07@users.noreply.github.com>
2023-08-18 13:54:36 +0700

    cmponent with banner
    cmponent with banner

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import React from 'react'
+import {Header,Content,Footer} from './func-components'
+import Calendar from './class-components'
 import Banner from './banner'
-export default function App(){
-    return <Banner/>
+function App() {
+    return (
+        <>
+        <Header/>
+        <p><center><Calendar/></center></p>
+        <Content/>
+        <Banner/>
+        <Footer/>
+        </> 
+       
+    );
 }
+
+export default App;
