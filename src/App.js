93bf05051661f692fae0d41612aa15401dd68716
Jakkaphat07 <110073963+Jakkaphat07@users.noreply.github.com>
2023-08-18 15:36:47 +0700

    update event-data.js
    update event-data.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from 'react'
-import Banner from './banner'
 import {EventData2} from './event-data'
-import {Calculator2} from './calculator'
-
 export default function App() {
     return <EventData2/> 
 }
