710705a3ed81236c83060642e7374bd2b55a449c
Jakkaphat07 <110073963+Jakkaphat07@users.noreply.github.com>
2023-10-06 15:13:56 +0700

    Form-Check
    create a react-form-hook

---
+++ lib/805.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+void main() => runApp(App805());
+class App805 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context)
+  => MaterialApp(home: HomePage());
+}
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => HomePageState();
+
+}
+class HomePageState extends State<HomePage> {
+  var _fontWeight = FontWeight.normal;
+  var _fontStyle = FontStyle.normal;
+  var _switchWeight = false;
+  var _switchStyle = false;
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+      appBar: AppBar(title: Text('Switch')),
+      body: Center(
+        child: Column(children: [
+          SizedBox(height: 30),
+          Text(
+            'Flutter',
+            textScaleFactor: 5.0,
+            style: TextStyle(
+              fontWeight: _fontWeight,
+              fontStyle: _fontStyle,
+            ),
+          ),
+          SizedBox(height: 30),
+          rowSwitchWeight(),
+          SizedBox(height: 20),
+          rowSwitchStyle()
+        ]),
+      ));
+  Widget rowSwitchWeight() =>
+      Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Bold ', textScaleFactor: 1.5),
+            Switch(
+                value: _switchWeight,
+                onChanged: (isOn) => setState(() {
+                  _switchWeight = isOn;
+                  if (isOn) {
+                    _fontWeight = FontWeight.bold;
+                  } else {
+                    _fontWeight = FontWeight.normal;
+                  }
+                }
+                )
+            ),
+          ]
+      );
+  Widget rowSwitchStyle() =>
+      Row(mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Italic ', textScaleFactor: 1.5),
+            Switch(
+                value: _switchStyle,
+                onChanged: (isOn) => setState(() {
+                  _switchStyle = isOn;
+                  _fontStyle = isOn ? FontStyle.italic :
+                  FontStyle.normal;
+                }
+                )
+            ),
+          ]
+      );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,81 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() => runApp(App803());
+
+class App803 extends StatelessWidget {
+  const App803({super.key});
+  @override
+  Widget build(BuildContext context) => MaterialApp(home: HomePage());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class HomePage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+  State<HomePage> createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  var _ctrlEmail = TextEditingController();
+  var _ctrlPswd = TextEditingController();
+  var _ctrlName = TextEditingController();
+  String _str = '';
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(title: Text('TextField')),
+    body: Center(
+      child: SizedBox(
+        width: 350,
+        child: Column(children: [
+          SizedBox(height: 30),
+          textFieldEmail(),
+          SizedBox(height: 20),
+          textFieldPswd(),
+          SizedBox(height: 20),
+          textFieldName(),
+          SizedBox(height: 40),
+          Text('$_str', textScaleFactor: 1.3)
+        ]),
       ),
-    );
-  }
+    ),
+  );
+  OutlineInputBorder outlineBorder() =>
+      OutlineInputBorder(borderSide: BorderSide(color: Colors.grey, width: 2));
+  TextStyle textStyle() => TextStyle(
+    color: Colors.indigo,
+    fontSize: 20,
+    fontWeight: FontWeight.normal, //ค่าดีฟอลต์อยู่แล้ว
+  );
+  Widget textFieldEmail() => TextField(
+    controller: _ctrlEmail,
+    decoration: InputDecoration(
+      border: outlineBorder(),
+      hintText: 'Email',
+    ),
+    keyboardType: TextInputType.emailAddress,
+    style: textStyle(),
+    onChanged: (text) => updateText(),
+  );
+  Widget textFieldPswd() => TextField(
+    controller: _ctrlPswd,
+    decoration: InputDecoration(
+      border: outlineBorder(),
+      hintText: 'Password',
+    ),
+    obscureText: true,
+    style: textStyle(),
+    onChanged: (text) => updateText(),
+  );
+  Widget textFieldName() => TextField(
+    controller: _ctrlName,
+    decoration: InputDecoration(
+      border: outlineBorder(),
+      hintText: 'Name',
+    ),
+    style: textStyle(),
+    onChanged: (text) => updateText(),
+  );
+  void updateText() => setState(() {
+    _str = 'Email: ${_ctrlEmail.value.text}\n';
+    _str += 'Pswd: ${_ctrlPswd.value.text}\n';
+    _str += 'Name: ${_ctrlName.value.text}';
+  });
 }
